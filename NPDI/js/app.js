0c877be17a9cc50ff2d56eb6875dac3e8a368ff1
LuizPPA <luizphilippep@gmail.com>
2016-06-26 18:34:24 -0300

    NPDI 1.6

--- NPDI/js/app.js
+++ NPDI/js/app.js
@@ -2,9 +2,7 @@ var app = angular.module('clock', []);
 
 app.controller('clockController', function($scope, $interval){
   $scope.currentTime = new Date().toLocaleTimeString();
-  $interval(theTime($scope), 1000);
+  $interval(function () {
+      $scope.currentTime = new Date().toLocaleTimeString();
+  }, 1000);
 });
-
-function theTime($scope){
-  $scope.currentTime = new Date().toLocaleTimeString();
-}
